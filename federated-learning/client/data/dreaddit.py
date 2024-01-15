2a080250d0f422f9256ccd05f750684b5a923942
Victor Martinez <correo.vmp@gmail.com>
2024-01-15 19:11:15 +0100

    Added Dreaddit and Mixed Depression datasets

--- federated-learning/client/data/dreaddit.py
+++ federated-learning/client/data/dreaddit.py
@@ -4,8 +4,8 @@ import transformers
 from torch.utils.data import Dataset, DataLoader
 
 
-def get_acl_dep_sad_dataloaders(root, tokenizer, train_size=0.8, eval_size=0.2, train_batch_size=4, eval_batch_size=2, max_len=512, seed=200):
-    train_dataset, eval_dataset = get_acl_dep_sad_datasets(
+def get_dreaddit_dataloaders(root, tokenizer, train_size=0.8, eval_size=0.2, train_batch_size=4, eval_batch_size=2, max_len=512, seed=200):
+    train_dataset, eval_dataset = get_dreaddit_datasets(
         root, tokenizer, train_size, eval_size, max_len, seed)
 
     train_params = {'batch_size': train_batch_size,
@@ -25,8 +25,8 @@ def get_acl_dep_sad_dataloaders(root, tokenizer, train_size=0.8, eval_size=0.2, 
     return train_loader, eval_loader
 
 
-def get_acl_dep_sad_test_dataloader(root, tokenizer, test_batch_size=2, max_len=512, seed=200):
-    test_dataset = get_acl_dep_sad_test_dataset(
+def get_dreaddit_test_dataloader(root, tokenizer, test_batch_size=2, max_len=512, seed=200):
+    test_dataset = get_dreaddit_test_dataset(
         root, tokenizer, max_len, seed)
 
     test_params = {'batch_size': test_batch_size,
@@ -38,15 +38,15 @@ def get_acl_dep_sad_test_dataloader(root, tokenizer, test_batch_size=2, max_len=
     return test_loader
 
 
-def get_acl_dep_sad_datasets(root, tokenizer, train_size=0.8, eval_size=0.2, max_len=512, seed=200):
-    train_dataframe, eval_dataframe = get_acl_dep_sad_dataframes(
+def get_dreaddit_datasets(root, tokenizer, train_size=0.8, eval_size=0.2, max_len=512, seed=200):
+    train_dataframe, eval_dataframe = get_dreaddit_dataframes(
         root, train_size, eval_size, seed)
-    train_dataset = AclDepressionSadness(train_dataframe, tokenizer, max_len)
-    eval_dataset = AclDepressionSadness(eval_dataframe, tokenizer, max_len)
+    train_dataset = Dreaddit(train_dataframe, tokenizer, max_len)
+    eval_dataset = Dreaddit(eval_dataframe, tokenizer, max_len)
     return train_dataset, eval_dataset
 
 
-def get_acl_dep_sad_dataframes(root, train_size=0.8, eval_size=0.2, seed=200):
+def get_dreaddit_dataframes(root, train_size=0.8, eval_size=0.2, seed=200):
     # Check if the sizes add up to 1
     if train_size + eval_size != 1:
         raise ValueError(
@@ -60,21 +60,20 @@ def get_acl_dep_sad_dataframes(root, train_size=0.8, eval_size=0.2, seed=200):
     eval_dataframe = df.drop(
         train_dataframe.index).reset_index(drop=True)
     train_dataframe = train_dataframe.reset_index(drop=True)
-
+    
     return train_dataframe, eval_dataframe
 
 
-def get_acl_dep_sad_test_dataset(root, tokenizer, max_len=512, seed=200):
+def get_dreaddit_test_dataset(root, tokenizer, max_len=512, seed=200):
     # Load the dataset
     test_df = pd.read_csv(root)
     test_dataframe = test_df.reset_index(drop=True)
 
-    test_dataset = AclDepressionSadness(test_dataframe, tokenizer, max_len)
+    test_dataset = Dreaddit(test_dataframe, tokenizer, max_len)
     return test_dataset
 
-
-# credits for the dataset to https://github.com/tiasa2/Interpretability-of-Federated-Learning-for-Fine-grained-Classification-of-Sadness-and-Depression
-class AclDepressionSadness(Dataset):
+# credits for the dataset to https://arxiv.org/pdf/1911.00133.pdf Stres vs no Stress
+class Dreaddit(Dataset):
     def __init__(self, dataframe, tokenizer, max_len):
         self.len = len(dataframe)
         self.data = dataframe
@@ -82,7 +81,7 @@ class AclDepressionSadness(Dataset):
         self.max_len = max_len
 
     def __getitem__(self, index):
-        text = str(self.data.tweet[index])
+        text = str(self.data.text[index])
         text = " ".join(text.split())
         inputs = self.tokenizer(
             text,
@@ -100,7 +99,7 @@ class AclDepressionSadness(Dataset):
         return {
             'input_ids': torch.tensor(ids, dtype=torch.long),
             'attention_mask': torch.tensor(mask, dtype=torch.long),
-            'label': torch.tensor(self.data.target[index])
+            'label': torch.tensor(self.data.label[index])
             # , dtype=torch.long)
         }
 
