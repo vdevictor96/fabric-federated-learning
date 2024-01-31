ef9bc4195448963f0e680ce193a53c48bbe59820
Victor Martinez <correo.vmp@gmail.com>
2024-01-31 12:10:52 +0100

    fix testing models

--- federated-learning/client/model/bert_tiny.py
+++ federated-learning/client/model/bert_tiny.py
@@ -19,9 +19,9 @@ def load_bert_tiny_model(model_path, device='cuda'):
     ml_mode = model_info['ml_mode']
     ml_mode_string = 'Centralised Machine Learning' if ml_mode == 'ml' else 'Federated Learning' if ml_mode == 'fl' else 'Blockchain-Based Federated Learning'
     if ml_mode == 'ml':
-        print(f"Loaded model bert-tiny from date {model_info['date']}. Trained with {model_mode_string} technology.\nEpoch {model_info['epoch']}, lr: {model_info['lr']}, optimizer: {model_info['optimizer']}\nTrain accuracy: {model_info['tr_acc']:.2f} %, Validation accuracy: {model_info['val_acc']:.2f} %")
+        print(f"Loaded model bert-tiny from date {model_info['date']}. Trained with {ml_mode_string} technology.\nEpoch {model_info['epoch']}, lr: {model_info['lr']}, optimizer: {model_info['optimizer']}\nTrain accuracy: {model_info['tr_acc']:.2f} %, Validation accuracy: {model_info['val_acc']:.2f} %")
     else:
-        print(f"Loaded model bert-tiny from date {model_info['date']}. Trained with {model_mode_string} technology.\nRound {model_info['round']}, lr: {model_info['lr']}, optimizer: {model_info['optimizer']}\nTrain accuracy: {model_info['tr_acc']:.2f} %, Validation accuracy: {model_info['val_acc']:.2f} %")
+        print(f"Loaded model bert-tiny from date {model_info['date']}. Trained with {ml_mode_string} technology.\nRound {model_info['round']}, lr: {model_info['lr']}, optimizer: {model_info['optimizer']}\nTrain accuracy: {model_info['tr_acc']:.2f} %, Validation accuracy: {model_info['val_acc']:.2f} %")
     model_state = model_info['model_state_dict']
     # Load the state dictionary into the model
     model.load_state_dict(model_state)
