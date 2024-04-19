f439f087006b021a15600996665ed690ef960801
Victor <correo.vmp@gmail.com>
2024-04-19 12:18:20 +0200

    added final dp experiments 3 seeds - pending to create 10 seeds

---
+++ federated-learning/client/config/phase-4/final/dp/duplicate-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIRECTORY=$1
+
+# Navigate to the directory
+cd "$DIRECTORY"
+
+# Loop through all files matching the pattern "fl_*.json"
+for FILE in fl_*.json; do
+    # Check if the file exists to avoid error messages
+    if [ -f "$FILE" ]; then
+        # Generate the new file name by prepending "bc" to the original file name
+        NEW_FILE_NAME="bc$FILE"
+        
+        # Duplicate the file with the new name, if it does not already exist
+        if [ ! -f "$NEW_FILE_NAME" ]; then
+            cp "$FILE" "$NEW_FILE_NAME"
+        else
+            echo "File $NEW_FILE_NAME already exists. Skipping."
+        fi
+    fi
+done
+
+echo "Files have been duplicated with the new naming convention."
